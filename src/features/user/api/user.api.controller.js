2f9a6a81b15efb21ca80a4b04dba4442c5323eea
Andrei Sousa <sousandrei@gmail.com>
2018-06-08 21:08:00 -0300

    user api: get finalized

--- src/features/user/api/user.api.controller.js
+++ src/features/user/api/user.api.controller.js
@@ -2,15 +2,80 @@ const User = require('../user.schema')
 
 exports.get = async (req, res) => {
 
-	let user = User
-		.query({})
-		.orderBy('-name')
-		.fetchPage({
-			pageSize: 5,
-			page: 1
-		})
+	let { id } = req.query
 
-	user = await user
+	if (!id)
+		return res.status(400).end()
 
-	return res.status(200).json(user)
+	if (isNaN(parseFloat(id)))
+		return res.status(400).end()
+
+	try {
+		let user = await User
+			.where({ id })
+			.fetch({
+				withRelated: [
+					{
+						'companies': function (qb) {
+							qb
+								.select(
+									'user_id',
+									'companies.id',
+									'companies.created_at',
+									'companies.name',
+									'contact_user as isContact')
+								.leftJoin(
+									'companies',
+									'teams.company_id',
+									'companies.id')
+						}
+					},
+					{
+						'applications': function (qb) {
+							qb
+								.select(
+									'user_id',
+									'applications.id',
+									'applications.created_at',
+									'listings.id as listings_id',
+									'listings.name',
+									'listings.description',
+									'cover_letter')
+								.innerJoin(
+									'listings',
+									'listings.id',
+									'applications.listing_id')
+						}
+					},
+					'createdListings'
+				]
+			})
+
+		user = user.toJSON()
+
+		for (let c of user.companies)
+			delete c.user_id
+
+		for (let a of user.applications) {
+			a.listing = {
+				id: a.listings_id,
+				name: a.name,
+				description: a.description
+			}
+
+			delete a.user_id
+			delete a.name
+			delete a.listings_id
+			delete a.description
+		}
+
+		for (let t of user.createdListings)
+			delete t.created_by
+
+		return res.status(200).json(user)
+	} catch (err) {
+		return res.status(500).json(err)
+	}
+
+
 }
