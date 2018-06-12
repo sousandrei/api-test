5a46594e33877e0873ae771c607ed4465e13e108
Andrei Sousa <sousandrei@gmail.com>
2018-06-12 12:01:28 -0300

    team feature cleanup

--- src/features/team/api/team.api.route.js
+++
@@ -1,9 +0,0 @@
-const { get } =
-	require('./team.api.controller')
-
-module.exports = async function (app) {
-
-	app.route('/api/teams')
-		.get(get)
-
-}
--- src/features/team/team.js
+++ src/features/team/team.js
@@ -1,7 +1,3 @@
 module.exports = {
-	model: require('./team.schema.js'),
-	api: {
-		controller: require('./api/team.api.controller.js'),
-		route: require('./api/team.api.route.js')
-	}
+	model: require('./team.schema.js')
 }
