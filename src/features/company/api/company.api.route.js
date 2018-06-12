3181b108bd847b9b657cd38f9101c26629d1adeb
Andrei Sousa <sousandrei@gmail.com>
2018-06-12 12:01:05 -0300

    company feature cleanup

--- src/features/company/api/company.api.route.js
+++
@@ -1,9 +0,0 @@
-const { get } =
-	require('./company.api.controller')
-
-module.exports = async function (app) {
-
-	app.route('/api/companies')
-		.get(get)
-
-}
--- src/features/company/company.js
+++ src/features/company/company.js
@@ -1,7 +1,3 @@
 module.exports = {
-	model: require('./company.schema.js'),
-	api: {
-		controller: require('./api/company.api.controller.js'),
-		route: require('./api/company.api.route.js')
-	}
+	model: require('./company.schema.js')
 }
